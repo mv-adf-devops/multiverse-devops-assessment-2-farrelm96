e3ba2f5d25086a49b1640473602e49bbf64ad3d4
farrelm96 <102584871+farrelm96@users.noreply.github.com>
2023-12-19 00:20:08 +0000

    Fix for validating Q3 where header was missing again

--- src/input.py
+++ src/input.py
@@ -6,6 +6,7 @@ from extract import capitalize_user_names
 from extract import remove_invalid_answer_3
 def main():
     filename="results.csv"
-    print(remove_invalid_answer_3(capitalize_user_names(remove_blank_rows(remove_duplicates(read_csv(filename))))))
+    cleaned_data = remove_invalid_answer_3(capitalize_user_names(remove_blank_rows(remove_duplicates(read_csv(filename)))))
+    print(cleaned_data)
 if __name__ =="__main__":
     sys.exit(main())
