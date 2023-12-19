185342dbc22271d55b199a909c91a6731f2dd2bd
farrelm96 <102584871+farrelm96@users.noreply.github.com>
2023-12-19 00:49:26 +0000

    output file created

--- src/extract/__init__.py
+++ src/extract/__init__.py
@@ -8,22 +8,17 @@ def read_csv(filename):
 def remove_duplicates(filename):
     # Create a set to store unique user_ids
     unique_user_ids = set()
-    
     # Create a new dataset to store the unique rows
     unique_dataset = [filename[0]]  # Copy the header
-    
     # Iterate through the rows of the dataset
     for row in filename[1:]:
         user_id = row[0]
-        
         # Check if the user_id is not in the set of unique user_ids
         if user_id not in unique_user_ids:
             # Add the row to the unique dataset
             unique_dataset.append(row)
-            
             # Add the user_id to the set of unique user_ids
-            unique_user_ids.add(user_id)
-    
+            unique_user_ids.add(user_id)    
     return unique_dataset
 
 def remove_blank_rows(filename):
@@ -48,10 +43,12 @@ def remove_invalid_answer_3(filename):
     # Filter rows where answer_3 is within the range [1, 10]
     filtered_dataset = [row for row in filename if row[answer_3_index].isdigit() and 1 <= int(row[answer_3_index]) <= 10]
     cleaned_dataset = [header]+ filtered_dataset
-
     return cleaned_dataset
 
-
-
-
-    
+def write_to_file(dataset, output_file):
+    with open(output_file, 'w') as file:
+        # Write the header
+        file.write(','.join(dataset[0]) + '\n')
+        # Write the data rows
+        for row in dataset[1:]:
+            file.write(','.join(row) + '\n')
